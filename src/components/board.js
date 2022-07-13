2ad094b61d0c30a0d74577d0d94aac410d058d4b
jeong922 <hwajeong922@naver.com>
2022-07-13 20:55:54 +0900

    board__input에 placeholder 추가

--- src/components/board.js
+++ src/components/board.js
@@ -4,7 +4,7 @@ export default class BoardComponent extends BaseComponent {
   constructor(title) {
     super(`<div class="board">
             <div class="board__title"></div>
-            <input type="text" class="board__input" />
+            <input type="text" class="board__input" placeholder="텍스트를 입력하세요." />
           </div>`);
 
     const boardTitle = this.element.querySelector('.board__title');
